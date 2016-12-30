b780559086a21bea6b582b9143127c64f106c25d
lixinmin <lixinmin@nibirutech.com>
2016-12-30 16:12:46 +0800

    problem addtwonum

---
+++ 2-add-two-numbers.go
@@ -0,0 +1,39 @@
+package leetcode_golang
+
+
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil || l2 == nil {
+		return nil
+	}
+	var vals []int
+	val := 0
+	for l1 != nil || l2 != nil || val != 0{
+		if l1 != nil {
+			val += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			val += l2.Val
+			l2 = l2.Next
+		}
+
+		vals = append(vals,val%10)
+		val = val / 10
+	}
+
+	head := &ListNode{vals[0], nil}
+	tail := head
+
+	for i := 1; i < len(vals); i ++ {
+		Node := &ListNode{vals[i], nil}
+		tail.Next = Node
+		tail = Node
+	}
+
+	return head
+}
